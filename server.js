bf4d309712f3529297590b2d805ac1d82119c9bb
David Kolia <Dkolia@icloud.com>
2018-07-09 14:48:29 -0500

    Completed chapter 1

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require("express")
 const app = express();
 
-app.listen(3000, ) => {
+//Greetings App
+app.get('/greeting/:name', (req, res) => {
+  // res.send({params: req.params, queries: req.query})
+
+// building a string, we will res.send back to our user
+  const output = "Stop " + req.params.name + "!";
+
+
+  res.send (output)
+
+
+});
+
+
+app.listen(3000, () => {
   console.log("Server is running on port: 3000");
 })
